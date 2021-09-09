d4f80fdd03349edae7ca6aad052b87ef56845bbf
HongSic <gpo04174@naver.com>
2021-09-09 17:15:31 +0900

    2021-09-09 Merge main->main

--- Web/WebHttpCookieValue.cs
+++ Web/WebHttpCookieValue.cs
@@ -29,12 +29,11 @@ namespace HSServer.Web
         public int? MaxAge { get; set; }
         public DateTime? Expires { get; set; }
 
-        public string SameSite { get; set; }
-        public bool? Session { get; set; }
-        public bool? HostOnly { get; set; }
-
         public string Path { get; set; }
         public string Domain { get; set; }
+
+        public string SameSite { get; set; }
+        public bool? Session { get; set; }
         public bool? HostOnly { get; set; }
         public bool? Secure { get; set; }
 
@@ -45,23 +44,10 @@ namespace HSServer.Web
             if (HttpOnly) sb.Append("; HttpOnly");
             if (Path != null) sb.Append("; Path=").Append(Path);
             if (Domain != null) sb.Append("; Domain=").Append(Domain);
-            if (HostOnly != null) sb.Append("; HostOnly=").Append(HostOnly);
-            if (Secure != null) sb.Append("; Secure=").Append(Secure);
-            if (MaxAge != null) sb.Append("; Max-Age=").Append(MaxAge);
-            if (Expires != null) sb.Append("; Expires=").Append(Expires.Value.ToString("R"));
-            return sb.ToString();
-        }
-
-        public override string ToString()
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.Append(Data);
-            if (HttpOnly) sb.Append("; HttpOnly");
-            if (Path != null) sb.Append("; Path=").Append(Path);
             if (Session != null) sb.Append("; Session=").Append(Session);
             if (HostOnly != null) sb.Append("; HostOnly=").Append(HostOnly);
             if (SameSite != null) sb.Append("; SameSite=").Append(SameSite);
-            if (Domain != null) sb.Append("; Domain=").Append(Domain);
+            if (Secure != null) sb.Append("; Secure=").Append(Secure);
             if (MaxAge != null) sb.Append("; Max-Age=").Append(MaxAge);
             if (Expires != null) sb.Append("; Expires=").Append(Expires.Value.ToString("R"));
             return sb.ToString();
