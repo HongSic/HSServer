08b3c839e44a68413b85f1579ecd23bbe50a40e1
HongSic <gpo04174@naver.com>
2022-03-15 06:40:59 +0900

    2022-03-15 Add Set function in IWebHttpHeaders

--- Web/IWebHttpHeaders.cs
+++ Web/IWebHttpHeaders.cs
@@ -8,6 +8,9 @@ namespace HSServer.Web
         void Add(string Key, string Value);
         void Add(string Key, params string[] Values);
         void Add(string Key, IList<string> Values);
+        void Set(string Key, string Value);
+        void Set(string Key, params string[] Values);
+        void Set(string Key, IList<string> Values);
         bool Remove(string Key);
         void Clear();
 
